fa7508b28a9771ca9d6eeb5fc315b9168d8ff49f
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-19 22:51:09 +0330

    add style to the main components

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Mamad</div>;
+  return <div className='hidden text-sm font-semibold md:block'>Mamad</div>;
 }
 
 export default Username;
