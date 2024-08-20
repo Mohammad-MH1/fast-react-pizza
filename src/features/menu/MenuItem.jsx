d9a4c6133ce6119964c4a5dc3f49097397895406
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-20 18:23:22 +0330

    create cartSlice and implement adding to cart in Menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,9 +1,25 @@
 import { formatCurrency } from '../../utils/helpers';
 
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className='flex gap-4 py-2'>
@@ -25,7 +41,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type='small'>Add to cart</Button>
+          {!soldOut && (
+            <Button type='small' onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
