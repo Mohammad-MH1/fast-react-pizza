d3b0edca0950a3daf83ed175b1a5994cae3ed44a
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-08-20 17:39:45 +0330

    use the username state in application

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = str =>
@@ -35,6 +36,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector(state => state.user.username);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
@@ -49,7 +51,13 @@ function CreateOrder() {
       <Form method='POST'>
         <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
           <label className='sm:basis-40'>First Name</label>
-          <input className='input grow' type='text' name='customer' required />
+          <input
+            className='input grow'
+            type='text'
+            name='customer'
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
